713847c6d94aaddd939aea23962dcc5a53463a75
Kenneth Yu <kyu22@buffalo.edu>
2019-01-07 14:49:20 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,8 +1,7 @@
-
-  def time
-    if hour < 12
-      puts "Good Morning!"
-    else 
-      puts "Good Afternoon"
-    end
-  end
+def time
+  if hour < 12
+    puts "Good Morning!"
+   else 
+    puts "Good Afternoon"
+   end
+end
