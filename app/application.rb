9444c74db7991b13847cb260977e6ba645ef60c9
Kenneth Yu <kyu22@buffalo.edu>
2019-01-07 14:56:34 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,10 +1,10 @@
 class Application
   def call(env)
-    #puts time
-    if Time.now.hour< 12
-      puts "Good Morning!"
-    else 
-      puts "Good Afternoon"
-    end
+    # #puts time
+    # if Time.now.hour < 12
+    #   puts "Good Morning!"
+    # else 
+    #   puts "Good Afternoon"
+    # end
   end
 end
